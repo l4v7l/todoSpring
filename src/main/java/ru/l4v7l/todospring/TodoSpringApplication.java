ebb7067b4450f238a5c8d245026bfecccba63107
l4v7l <vovan_lipkin@mail.ru>
2025-01-23 20:23:30 +0300

    init commit

---
+++ src/main/java/ru/l4v7l/todospring/TodoSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.l4v7l.todospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSpringApplication.class, args);
+    }
+
+}
